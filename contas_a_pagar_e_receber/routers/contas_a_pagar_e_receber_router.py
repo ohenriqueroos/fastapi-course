7c129ae73951e032e35682c58c5af92e5a9d0cdd
ohenriqueroos <rooshenriquematheus@gmail.com>
2024-11-24 15:36:45 -0300

    third class - connect to database

--- contas_a_pagar_e_receber/routers/contas_a_pagar_e_receber_router.py
+++ contas_a_pagar_e_receber/routers/contas_a_pagar_e_receber_router.py
@@ -1,8 +1,13 @@
 from decimal import *
-
-from fastapi import APIRouter
-from pydantic import BaseModel
 from typing import List
+
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+from shared.dependencies import get_db
+
+from contas_a_pagar_e_receber.models.conta_a_pagar_receber_model import ContaPagarReceber
 
 router = APIRouter(prefix="/contas-a-pagar-e-receber")
 
@@ -13,33 +18,28 @@ class ContaPagarReceberResponse(BaseModel):
     valor: Decimal
     tipo: str  # PAGAR, RECEBER
 
+
 class ContaPagarReceberRequest(BaseModel):
     descricao: str
     valor: Decimal
     tipo: str  # PAGAR, RECEBER
 
+class Config:
+    orm_mode = True
+
+
 @router.get("", response_model=List[ContaPagarReceberResponse])
-def listar_contas():
-    return [
-        ContaPagarReceberResponse(
-            id=1,
-            descricao="Aluguel",
-            valor=1000.50,
-            tipo="PAGAR"
-        ),
-        ContaPagarReceberResponse(
-            id=2,
-            descricao="Salário",
-            valor=5000,
-            tipo="RECEBER"
-        )
-    ]
+def listar_contas(db: Session = Depends(get_db)) -> List[ContaPagarReceberResponse]:
+    return db.query(ContaPagarReceber).all()
 
 @router.post("", response_model=ContaPagarReceberResponse, status_code=201)
-def criar_conta(conta: ContaPagarReceberRequest):
-    return ContaPagarReceberResponse(
-        id=3,
-        descricao=conta.descricao,
-        valor=conta.valor,
-        tipo=conta.tipo
+def criar_conta(conta_a_pagar_e_receber_request: ContaPagarReceberRequest, db: Session = Depends(get_db)) -> ContaPagarReceberResponse:
+    contas_a_pagar_e_receber = ContaPagarReceber(
+        **conta_a_pagar_e_receber_request.model_dump()
     )
+
+    db.add(contas_a_pagar_e_receber)
+    db.commit()
+    db.refresh(contas_a_pagar_e_receber)
+
+    return contas_a_pagar_e_receber
